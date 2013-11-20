d651d077702bb08f2c9c5311c1226eabbf5b3b2d
Gábor Görzsöny <gabor@gorzsony.com>
2013-11-20 03:22:49 +0100

    exit_program command is lambda again, just to test script_engine

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 #include "gglib.hpp"
 
-void exit_program(int exit_code)
-{
-    gg::console* con = gg::console::get_invoker_console();
-    if (con == nullptr) std::exit(exit_code);
-    else con->get_app()->exit(exit_code);
-}
-
 int main()
 {
     gg::application* app = gg::application::create_instance("test app", 0, 1);
@@ -16,15 +9,19 @@ int main()
     con->open();
 
 
-    app->get_script_engine()->add_function("exit_program", exit_program);
+    app->get_script_engine()->add_function("exit_program",
+            [](int exit_code) { gg::console::get_invoker_console()->get_app()->exit(exit_code); });
 
-    app->get_script_engine()->add_function("echo", [](std::string str) { return str; }, true);
+    app->get_script_engine()->add_function("echo",
+            [](std::string str) { return str; });
 
     app->get_script_engine()->add_function("is_integer",
-            [](std::string i){ return (gg::util::is_integer(i) ? "true" : "false"); });
+            [](std::string i){ return (gg::util::is_integer(i) ? "true" : "false"); },
+            true);
 
     app->get_script_engine()->add_function("is_float",
-            [](std::string i){ return (gg::util::is_float(i) ? "true" : "false"); });
+            [](std::string i){ return (gg::util::is_float(i) ? "true" : "false"); },
+            true);
 
     app->get_script_engine()->add_function("sum",
             [](gg::varlist vl)
